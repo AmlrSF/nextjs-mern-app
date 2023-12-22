aa14efae7ceaf013bfd6c63a83192e78b2be7607
Amir souaf <111384104+AmirSouaF@users.noreply.github.com>
2023-12-22 09:54:53 -1100

    Update user.ts

--- lib/validations/user.ts
+++ lib/validations/user.ts
@@ -2,7 +2,7 @@ import * as z from "zod";
 
 
 export  const userValidationformSchema  = z.object({
-    image:z.string().url(),
+    image:z.string().url() ,
     name:z.string().min(3).max(30),
     username:z.string().min(3).max(30),
     bio:z.string().min(3).max(1000),
